183de25c453117c012b9bf803154b55b959180ad
kevin russel <kzone159@gmail.com>
2026-05-03 00:12:41 -0500

    adding code

--- front_end.py
+++ front_end.py
@@ -4,16 +4,27 @@ import tkinter as tk
 
 def create_window(root):
     root.title("UDP")
-    root.geometry("1500x1000")
+    root.geometry("1600x1000")
     return root
 
-
+def create_box(root):
+    frame = tk.Frame(
+        root,
+        bg="white",
+        width=1200,
+        height=300,
+        relief="solid",   # gives it a border
+        borderwidth=1
+    )
+    frame.place(x = 200, y = 50)
+    return frame
 
 def main():
     root = tk.Tk()
     create_window(root)
 
     root.configure(background="grey")
+    box = create_box(root)
     root.mainloop()
 
 if __name__ == '__main__':
