477b69b84425e571162611b09c9afd880828f90f
kevin russel <kzone159@gmail.com>
2026-05-03 16:20:59 -0500

    adding a UDP server class

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ class UDP_Client:
     import socket
 
     def send(self):
-        udp_client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        udp_client_socket = self.socket.socket(self.socket.AF_INET,self.socket.SOCK_DGRAM)
         udp_server_address = ("127.0.0.1",8080)
         message = b"Hello world"
 
