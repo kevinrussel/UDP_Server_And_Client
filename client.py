8679b8055f37b601db802e83003506adb5e2661b
kevin russel <kzone159@gmail.com>
2026-05-03 16:48:47 -0500

    adding innit for client

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 class UDP_Client:
     import socket
-
+    def __init__(self):
+        self.udp_client_socket = self.socket.socket(self.socket.AF_INET,self.socket.SOCK_DGRAM)
+        self.udp_server_address = ("127.0.0.1",8080)
+    
+    
     def send(self):
-        udp_client_socket = self.socket.socket(self.socket.AF_INET,self.socket.SOCK_DGRAM)
-        udp_server_address = ("127.0.0.1",8080)
         message = b"Hello world"
-
-        udp_client_socket.sendto(message,udp_server_address)
+        self.udp_client_socket.sendto(message,self.udp_server_address)
