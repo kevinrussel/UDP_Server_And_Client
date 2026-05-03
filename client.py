1c6956ef866cff26a0a9d435d0d13d46f362782c
kevin russel <kzone159@gmail.com>
2026-05-03 17:04:46 -0500

    adding function signitures

--- client.py
+++ client.py
@@ -2,9 +2,9 @@ class UDP_Client:
     import socket
     def __init__(self):
         self.udp_client_socket = self.socket.socket(self.socket.AF_INET,self.socket.SOCK_DGRAM)
-        self.udp_server_address = ("127.0.0.1",8080)
+        
     
     
-    def send(self):
+    def send(self, server_address = "127.0.0.1",port=8080):
         message = b"Hello world"
-        self.udp_client_socket.sendto(message,self.udp_server_address)
+        self.udp_client_socket.sendto(message,(server_address,port))
