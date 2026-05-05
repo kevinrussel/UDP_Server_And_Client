6ca22e1ffe3bc654ab76f428e96b7c39c35fbbb0
kevin russel <kzone159@gmail.com>
2026-05-04 22:55:40 -0500

    addig struct

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 
 udp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -8,7 +9,9 @@ udp_server_socket.bind(('',8080))
 while True:
     message,address = udp_server_socket.recvfrom(1024)
 
-    
-    print(message)
+    header = message[:1]
+
+    message = message[1:]
+    print(header)
     message = message.decode("utf-8")
     print(message)
