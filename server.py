7cf88b7c11dbaa9cfee3fdf2ba980b18bc6ffc55
kevin russel <kzone159@gmail.com>
2026-05-06 23:07:13 -0500

    adding code

--- server.py
+++ server.py
@@ -10,21 +10,17 @@ class udp_server:
 
     def handle_recieved_message(self,message):
         header = message[:9]
-        packet_num = struct.unpack("!Bd", header)[1]
-        timestamp = (self.time.time())
+        packet_num = struct.unpack("!Bd", header)[0]
+        timestamp = (self.time.time()) - struct.unpack("!Bd", header)[1]
         message = (message[9:]).decode("utf-8")
-        return   
+        return packet_num,timestamp,message 
     def udp_server_listen(self):
         num = 1
         while True:
-            message,address = udp_server_socket.recvfrom(1024)
-
-            header = message[:9]
-            header = struct.unpack("!Bd",header)[1]
-            
-            message = message[9:]
-            message = message.decode("utf-8")
-            print(f' This packet is {header}')
+            message,address = self.udp_server_socket.recvfrom(1024)
+            packet_num,timestamp,message = self.handle_recieved_message(message)
+            print(f' This packet is {packet_num}')
+            print(f'This is the time it took {timestamp}')
             print(message)                   
             print(f'Total Num of Recived packet is {num}\n')
             num = num + 1
