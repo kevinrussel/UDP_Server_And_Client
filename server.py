a53c953487572489774f702f1026ec30935bed18
kevin russel <kzone159@gmail.com>
2026-05-06 23:00:31 -0500

    adding code

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
 import socket
 import struct
 
+class udp_server:
 
-udp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-udp_server_socket.bind(('',8080))
 
-num = 1
-while True:
-    message,address = udp_server_socket.recvfrom(1024)
+    def start_udp_server():
+        udp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        udp_server_socket.bind(('',8080))
 
-    header = message[:5]
-    header = struct.unpack("!Bd",header)[1]
-    
-    message = message[5:]
-    message = message.decode("utf-8")
-    print(f' This packet is {header}')
-    print(message)                   
-    print(f'Total Num of Recived packet is {num}\n')
-    num = num + 1
+    num = 1
+    while True:
+        message,address = udp_server_socket.recvfrom(1024)
+
+        header = message[:9]
+        header = struct.unpack("!Bd",header)[1]
+        
+        message = message[9:]
+        message = message.decode("utf-8")
+        print(f' This packet is {header}')
+        print(message)                   
+        print(f'Total Num of Recived packet is {num}\n')
+        num = num + 1
+
+    if __name__ =='__main__':
+        sart_udp_server()
