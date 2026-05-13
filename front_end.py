2b464f0b85474dd9cb2414f5395dcf52f002011f
Kevin Russel <kzone159@gmail.com>
2026-05-12 22:10:33 -0500

    adding code

--- front_end.py
+++ front_end.py
@@ -120,10 +120,13 @@ def main():
         
 
     def use_last():
+        ip_input.delete(0, tk.END)
+        port_input.delete(0, tk.END)
+        ip,port = client.set_known_json_value(0)
+        ip_input.insert(0, ip)
+        port_input.insert(0, port)
+        client.set_udp_values("127.0.0.1", 8080)
 
-
-        
-        pass  # wire to your JSON loader
 
     def add_connection():
         pass  # wire to your JSON saver
