fd7a0862611a8579389bdcccf593321798561cf0
Kevin Russel <kzone159@gmail.com>
2026-05-13 09:38:42 -0500

    adding code

--- client.py
+++ client.py
@@ -54,10 +54,14 @@ class UDP_Client:
 
     def send_packets(self):
         sending_packets = self.get_drop_packets()
-
+        header_num = 0
+        server_address, port = self.get_udp_values()
         for i in range(0,101):
             num = self.random.randint(0,100)
             if (num <= sending_packets):
+                continue
+            message = self.create_header(i) + b"This is a Test Packet"
+            self.udp_client_socket.sendto(message,(server_address,port))
 
 
     def test(self,server_address, port):
