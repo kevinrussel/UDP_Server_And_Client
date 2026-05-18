27d941e7e22e7b4be19b4de50187743a897c421a
Kevin Russel <kzone159@gmail.com>
2026-05-18 09:33:15 -0500

    adding code

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
+import socket
+import struct
+import time
+import threading
+from  concurrent.futures import ThreadPoolExecutor
 
 class udp_server:
-    import socket
-    import struct
-    import time
-    import threading
     
-
-    def handle_recieved_message(self,message,num):
+    def handle_recieved_message(self,message):
         header = message[:10]
         unpacked = self.struct.unpack("!HD",header)
         packet_num = self.struct.unpack("!Hd", header)[0]
@@ -15,16 +15,16 @@ class udp_server:
         print(f' This packet is {packet_num}')
         print(f'Time for packet travel: {timestamp}')
         print(message)                   
-        print(f'Total Num of Recived packet is {num}\n')
         return packet_num,timestamp,message 
     
 
     def udp_server_listen(self):
         num = 1
-        while True:
-            message,address = self.udp_server_socket.recvfrom(1024)
-            # self.threading.Thread(target=self.handle_recieved_message, args = (message,num), daemon=True).start()
-            num = num + 1
+        with self.ThreadPoolExecutor(max_workers=8) as executor:
+            while True:
+                message,address = self.udp_server_socket.recvfrom(1024)
+                executor.submit(self.handle_recieved_message,message)
+                num = num + 1
 
     def start_udp_server(self):
         self.udp_server_socket = self.socket.socket(self.socket.AF_INET,self.socket.SOCK_DGRAM)
