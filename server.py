a3cb1b6b22941e5c2ce259ef7be6a55d60deec01
Kevin Russel <kzone159@gmail.com>
2026-05-18 09:34:38 -0500

    getting rid of class level imports

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ class udp_server:
         header = message[:10]
         unpacked = self.struct.unpack("!HD",header)
         packet_num = self.struct.unpack("!Hd", header)[0]
-        timestamp = (self.time.time()) - self.struct.unpack("!Hd", header)[1]
+        timestamp = (time.time()) - self.struct.unpack("!Hd", header)[1]
         message = (message[10:]).decode("utf-8")
         print(f' This packet is {packet_num}')
         print(f'Time for packet travel: {timestamp}')
@@ -20,14 +20,14 @@ class udp_server:
 
     def udp_server_listen(self):
         num = 1
-        with self.ThreadPoolExecutor(max_workers=8) as executor:
+        with ThreadPoolExecutor(max_workers=8) as executor:
             while True:
                 message,address = self.udp_server_socket.recvfrom(1024)
                 executor.submit(self.handle_recieved_message,message)
                 num = num + 1
 
     def start_udp_server(self):
-        self.udp_server_socket = self.socket.socket(self.socket.AF_INET,self.socket.SOCK_DGRAM)
+        self.udp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         self.udp_server_socket.bind(('',8080))
         self.udp_server_listen()
 
