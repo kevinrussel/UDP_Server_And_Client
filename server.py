0714c8afae2944f741a4fb6a085e623b1c181ccb
Kevin Russel <kzone159@gmail.com>
2026-05-26 21:46:54 -0500

    fixing futures

--- server.py
+++ server.py
@@ -11,9 +11,7 @@ class udp_server:
         packet_num,timestamp = struct.unpack("!Hd", header)
         timestamp = time.time() - timestamp
         message = (message[10:]).decode("utf-8")
-        print(packet_num)
-        print(timestamp)
-        print(message)                   
+                   
         return packet_num,timestamp,message 
     
 
@@ -24,13 +22,12 @@ class udp_server:
             while True:
                 try:
                     message,address = self.udp_server_socket.recvfrom(100)
-                    self.handle_recieved_message(message)
                     futures.append(executor.submit(self.handle_recieved_message,message))
                     num = num + 1
                 except KeyboardInterrupt:
                     break
         print("\n")
-        print(futures[0])
+        print(futures[0].result())
         print(num)
 
     def start_udp_server(self):
